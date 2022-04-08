4c29550ea6981662542daa0eab0074f671074558
kangyongbong <rkdqhd11@hanmail.net>
2022-04-08 15:15:20 +0900

    refactor : 다양한 연관관계 매핑

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,2 +1,22 @@
-package jpabook.jpashop.domain;public class Delivery {
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String city;
+    private String streat;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
 }
